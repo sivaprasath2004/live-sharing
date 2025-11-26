e4a326e15b69ee30b14c5ed2fe40ddc8f1e8c396
sivaprasath2004 <prasathsiva2004@gmail.com>
2025-11-26 21:38:28 +0530

    Home screen design

--- src/renderer/Menu.js
+++ src/renderer/Menu.js
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import DownloadButton from './Ui/Download.File.Button'
+import {Button} from 'antd'
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <div style={{display:'flex',alignItems:'center',height:'100vh'}}>
+      <div style={{background:'red',width:'50%',height:'100%'}}></div>
+      <div style={{background:"white",width:'50%',height:'100%',justifyContent:'center',alignItems:'center'}}>
+        <div style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
+          <Button>
+             Create Session
+          </Button>
+          <DownloadButton />
+          <Button>
+            Join Session 
+          </Button>
+        </div>
+      </div>
+    </div>
   )
 }
 
